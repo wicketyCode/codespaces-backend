1b9537ec55b7f276c06eb0f13967dfae4ca7f4fb
Erol Eskici <aberec@almbrand.dk>
2022-12-28 21:04:59 +0100

    more mock

--- src/products/Products.ts
+++ src/products/Products.ts
@@ -1,22 +1,124 @@
 const products = [
-    {
-      id: 1,
-      name: 'T-shirt',
-      price: 25,
-      description: 'A comfortable cotton t-shirt'
-    },
-    {
-      id: 2,
-      name: 'Jeans',
-      price: 50,
-      description: 'A classic pair of jeans'
-    },
-    {
-      id: 3,
-      name: 'Dress',
-      price: 75,
-      description: 'A stylish dress for any occasion'
-    }
-  ];
+  {
+    id: 1,
+    name: 'T-shirt',
+    price: 25,
+    description: 'A comfortable cotton t-shirt'
+  },
+  {
+    id: 2,
+    name: 'Jeans',
+    price: 50,
+    description: 'A classic pair of jeans'
+  },
+  {
+    id: 3,
+    name: 'Dress',
+    price: 75,
+    description: 'A stylish dress for any occasion'
+  },
+  {
+    id: 4,
+    name: 'Sneakers',
+    price: 100,
+    description: 'A pair of comfortable sneakers'
+  },
+  {
+    id: 5,
+    name: 'Jacket',
+    price: 125,
+    description: 'A warm and stylish jacket'
+  },
+  {
+    id: 6,
+    name: 'Hat',
+    price: 15,
+    description: 'A fashionable hat'
+  },
+  {
+    id: 7,
+    name: 'Bag',
+    price: 30,
+    description: 'A stylish and practical bag'
+  },
+  {
+    id: 8,
+    name: 'Gloves',
+    price: 20,
+    description: 'A pair of warm gloves'
+  },
+  {
+    id: 9,
+    name: 'Socks',
+    price: 10,
+    description: 'A comfortable pair of socks'
+  },
+  {
+    id: 10,
+    name: 'Scarf',
+    price: 20,
+    description: 'A cozy scarf'
+  },
+  {
+    id: 11,
+    name: 'Belt',
+    price: 15,
+    description: 'A stylish belt'
+  },
+  {
+    id: 12,
+    name: 'Skirt',
+    price: 40,
+    description: 'A trendy skirt'
+  },
+  {
+    id: 13,
+    name: 'Shirt',
+    price: 35,
+    description: 'A stylish shirt'
+  },
+  {
+    id: 14,
+    name: 'Pants',
+    price: 45,
+    description: 'A comfortable pair of pants'
+  },
+  {
+    id: 15,
+    name: 'Sweater',
+    price: 60,
+    description: 'A cozy sweater'
+  },
+  {
+    id: 16,
+    name: 'Shoes',
+    price: 80,
+    description: 'A stylish pair of shoes'
+  },
+  {
+    id: 17,
+    name: 'Blouse',
+    price: 50,
+    description: 'A stylish blouse'
+  },
+  {
+    id: 18,
+    name: 'Cardigan',
+    price: 55,
+    description: 'A comfortable cardigan'
+  },
+  {
+    id: 19,
+    name: 'Coat',
+    price: 120,
+    description: 'A warm and stylish coat'
+  },
+  {
+    id: 20,
+    name: 'Jumpsuit',
+    price: 100,
+    description: 'A warm and stylish Jumpsuit'
+  }
+];
 
-  export default products;
+export default products;
