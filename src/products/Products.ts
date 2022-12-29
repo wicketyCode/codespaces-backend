866d6bf5b1fcfa4972519ad91314558272019e92
Erol Eskici <aberec@almbrand.dk>
2022-12-30 00:23:20 +0100

    product mock changed

--- src/products/Products.ts
+++ src/products/Products.ts
@@ -1,124 +1,164 @@
 const products = [
   {
     id: 1,
-    name: 'T-shirt',
+    name: "T-shirt",
     price: 25,
-    description: 'A comfortable cotton t-shirt'
+    description: "A comfortable cotton t-shirt",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
   },
   {
     id: 2,
-    name: 'Jeans',
+    name: "Jeans",
     price: 50,
-    description: 'A classic pair of jeans'
+    description: "A classic pair of jeans",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
   },
   {
     id: 3,
-    name: 'Dress',
+    name: "Dress",
     price: 75,
-    description: 'A stylish dress for any occasion'
+    description: "A stylish dress for any occasion",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg",
   },
   {
     id: 4,
-    name: 'Sneakers',
+    name: "Sneakers",
     price: 100,
-    description: 'A pair of comfortable sneakers'
+    description: "A pair of comfortable sneakers",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
   },
   {
     id: 5,
-    name: 'Jacket',
+    name: "Jacket",
     price: 125,
-    description: 'A warm and stylish jacket'
+    description: "A warm and stylish jacket",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
   },
   {
     id: 6,
-    name: 'Hat',
+    name: "Hat",
     price: 15,
-    description: 'A fashionable hat'
+    description: "A fashionable hat",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg",
   },
   {
     id: 7,
-    name: 'Bag',
+    name: "Bag",
     price: 30,
-    description: 'A stylish and practical bag'
+    description: "A stylish and practical bag",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
   },
   {
     id: 8,
-    name: 'Gloves',
+    name: "Gloves",
     price: 20,
-    description: 'A pair of warm gloves'
+    description: "A pair of warm gloves",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
   },
   {
     id: 9,
-    name: 'Socks',
+    name: "Socks",
     price: 10,
-    description: 'A comfortable pair of socks'
+    description: "A comfortable pair of socks",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg",
   },
   {
     id: 10,
-    name: 'Scarf',
+    name: "Scarf",
     price: 20,
-    description: 'A cozy scarf'
+    description: "A cozy scarf",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
   },
   {
     id: 11,
-    name: 'Belt',
+    name: "Belt",
     price: 15,
-    description: 'A stylish belt'
+    description: "A stylish belt",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
   },
   {
     id: 12,
-    name: 'Skirt',
+    name: "Skirt",
     price: 40,
-    description: 'A trendy skirt'
+    description: "A trendy skirt",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg",
   },
   {
     id: 13,
-    name: 'Shirt',
+    name: "Shirt",
     price: 35,
-    description: 'A stylish shirt'
+    description: "A stylish shirt",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
   },
   {
     id: 14,
-    name: 'Pants',
+    name: "Pants",
     price: 45,
-    description: 'A comfortable pair of pants'
+    description: "A comfortable pair of pants",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
   },
   {
     id: 15,
-    name: 'Sweater',
+    name: "Sweater",
     price: 60,
-    description: 'A cozy sweater'
+    description: "A cozy sweater",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg",
   },
   {
     id: 16,
-    name: 'Shoes',
+    name: "Shoes",
     price: 80,
-    description: 'A stylish pair of shoes'
+    description: "A stylish pair of shoes",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
   },
   {
     id: 17,
-    name: 'Blouse',
+    name: "Blouse",
     price: 50,
-    description: 'A stylish blouse'
+    description: "A stylish blouse",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
   },
   {
     id: 18,
-    name: 'Cardigan',
+    name: "Cardigan",
     price: 55,
-    description: 'A comfortable cardigan'
+    description: "A comfortable cardigan",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-03.jpg",
   },
   {
     id: 19,
-    name: 'Coat',
+    name: "Coat",
     price: 120,
-    description: 'A warm and stylish coat'
+    description: "A warm and stylish coat",
+    image:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
   },
   {
     id: 20,
-    name: 'Jumpsuit',
+    name: "Jumpsuit",
     price: 100,
-    description: 'A warm and stylish Jumpsuit'
-  }
+    description: "A warm and stylish Jumpsuit",
+    image:
+    "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+  },
 ];
 
 export default products;
