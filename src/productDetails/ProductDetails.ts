968b4470a8a4c8715ffe04a317c9416590f22108
Erol Eskici <aberec@almbrand.dk>
2023-01-05 08:03:46 +0100

    product mock changed

--- src/productDetails/ProductDetails.ts
+++ src/productDetails/ProductDetails.ts
@@ -5,14 +5,20 @@ const productDetails = [
         price: 25,
         description: "A comfortable cotton t-shirt",
         images: [
-            "https://picsum.photos/id/10/200/300",
+            "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
             "https://picsum.photos/id/11/200/300",
             "https://picsum.photos/id/12/200/300",
+        ],
+        colors: [
+            { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+            { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+            { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
         ],
         availableSizes: ["S", "M", "L", "XL"],
         material: "Cotton",
         brand: "Nike",
         madeIn: "USA",
+        reviewsTotal: { href: '#', average: 5, totalCount: 240 },
         reviews: [
             {
                 user: "John",
@@ -32,14 +38,20 @@ const productDetails = [
         price: 50,
         description: 'A classic pair of jeans',
         images: [
-            'https://picsum.photos/id/20/200/300',
+            'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
             'https://picsum.photos/id/21/200/300',
             'https://picsum.photos/id/22/200/300'
+        ],
+        colors: [
+            { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+            { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+            { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
         ],
         availableSizes: ['28', '30', '32', '34'],
         material: 'Denim',
         brand: 'Levis',
         madeIn: 'Mexico',
+        reviewsTotal: { href: '#', average: 3, totalCount: 54 },
         reviews: [
             {
                 user: 'Mike',
@@ -59,14 +71,20 @@ const productDetails = [
         price: 75,
         description: 'A stylish dress for any occasion',
         images: [
-            'https://picsum.photos/id/30/200/300',
+            'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
             'https://picsum.photos/id/31/200/300',
             'https://picsum.photos/id/32/200/300'
+        ],
+        colors: [
+            { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+            { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+            { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
         ],
         availableSizes: ['XS', 'S', 'M', 'L', 'XL'],
         material: 'Polyester',
         brand: 'Calvin Klein',
         madeIn: 'China',
+        reviewsTotal: { href: '#', average: 4, totalCount: 117 },
         reviews: [
             {
                 user: 'Emily',
